1538d40c1f56e87f750b4585c2867b65505393f2
status102 <102887808+status102@users.noreply.github.com>
2024-04-09 09:58:38 +0800

    perf: 迁移hook

--- Cashier/Cashier.cs
+++ Cashier/Cashier.cs
@@ -26,6 +26,7 @@ namespace Cashier
 		public Configuration Config { get; init; }
 		public DalamudPluginInterface PluginInterface { get; init; }
 		public uint homeWorldId = 0;
+		private HookHelper HookHelper { get; init; } 
 
 		public Cashier(DalamudPluginInterface pluginInterface)
 		{
@@ -76,11 +77,7 @@ namespace Cashier
 
 				Svc.AddonEventManager.AddEvent((nint)addon, (nint)targetNode, AddonEventType.InputReceived, Test);
 			}
-			//_resetSlotHook?.Enable();
-			_setSlotItemIdHook?.Enable();
-			_tradeTargetHook?.Enable();
-			_tradeOtherMoney?.Enable();
-			//_tradeCountHook?.Enable();
+			HookHelper = new(this);
 		}
 
 		private void Test(AddonEventType atkEventType, nint atkUnitBase, nint atkResNode)
@@ -90,11 +87,7 @@ namespace Cashier
 
 		public void Dispose()
 		{
-			_tradeCountHook?.Dispose();
-			_tradeOtherMoney?.Dispose();
-			_tradeTargetHook?.Dispose();
-			_resetSlotHook?.Dispose();
-			_setSlotItemIdHook?.Dispose();
+			HookHelper.Dispose();
 			TaskManager!.Abort();
 			ECommonsMain.Dispose();
 
@@ -161,85 +154,6 @@ namespace Cashier
 		private void OnLogout()
 		{
 			homeWorldId = 0;
-		}
-
-		private delegate nint ResetSlot(nint a);
-
-		[Signature("40 53 48 83 EC 20 83 79 70 00 48 8B D9 74 36", DetourName = nameof(DetourResetSlot))]
-		private Hook<ResetSlot>? _resetSlotHook;
-
-		private nint DetourResetSlot(nint a)
-		{
-			PluginLog.Information($"一个格子被清空: {a:X}");
-
-			try {
-				// your plugin logic goes here.
-			} catch (Exception ex) {
-				PluginLog.Error("An error occured when handling a macro save event." + ex.Message);
-			}
-
-			return this._resetSlotHook!.Original(a);
-		}
-
-		private delegate nint SetSlotItemId(nint a, uint itemId, nint a3, int a4, nint a5);
-		[Signature("48 89 5C 24 08 48 89 6C 24 10 48 89 74 24 18 48 89 7C 24 20 41 56 48 83 EC 20 8B F2 41 8B E9 48 8B D9 45", DetourName = nameof(DetourSetSlotItemId))]
-		private Hook<SetSlotItemId>? _setSlotItemIdHook;
-
-		private nint DetourSetSlotItemId(nint a, uint itemId, nint a3, int a4, nint a5)
-		{
-			// 未能读取到物品数量，暂时搁置
-			PluginLog.Information($"一个格子设置: {a:X}, itemId:{itemId}, a3:{a3}, a4:{a4}, a5:{a5}");
-
-			try {
-				// your plugin logic goes here.
-			} catch (Exception ex) {
-				PluginLog.Error("An error occured when handling a macro save event." + ex.Message);
-			}
-
-			return this._setSlotItemIdHook!.Original(a, itemId, a3, a4, a5);
-		}
-
-		[Signature("4C 8B DC 53 55 48 81 EC 68 01 00 00 48 8B 05 0D 56 A6 01 48 33 C4 48 89 84 24 40 01 00 00", DetourName = nameof(DetourTradeTarget))]
-		private Hook<TradeTarget>? _tradeTargetHook;
-		private delegate nint TradeTarget(nint a1, nint a2, nint a3);
-		private nint DetourTradeTarget(nint a1, nint objectId, nint a3)
-		{
-			switch (Marshal.ReadByte(a3 + 4)) {
-				case 16:
-					// case 16: 交易状态更新
-					PluginUi.Trade.SetTradeTarget(objectId);
-					break;
-				case 17:
-					// case 17: 交易成功
-					break;
-				default:
-					PluginLog.Information($"交易目标: {a1:X}, {objectId:X}, {a3:X}, {Marshal.ReadByte(a3 + 4)}");
-					// case 7: 取消交易
-					// case 1: 别人交易你
-					// case 2: 发起交易
-
-					// case 5: 最终确认
-					break;
-			}
-			return this._tradeTargetHook!.Original(a1, objectId, a3);
-		}
-
-		[Signature("0F B7 42 06 4C 8B D1 44 8B 4A 10 4C 6B C0 38 41 80 BC 08", DetourName = nameof(DetourTradeOtherMoney))]
-		private Hook<TradeOtherMoney>? _tradeOtherMoney;
-		private delegate nint TradeOtherMoney(nint a1, nint a2);
-		private nint DetourTradeOtherMoney(nint a1, nint a2)
-		{
-			PluginLog.Information($"交易对面出价: {a1:X}, {a2:X}, {(uint)Marshal.ReadInt32(a2 + 8)}");
-			return this._tradeOtherMoney!.Original(a1, a2);
-		}
-
-		[Signature("3B 51 08 7D ?? 48 8B 41 20 48 63 D2 44 39 04 90", DetourName = nameof(DetourTradeCount))]
-		private Hook<TradeCount>? _tradeCountHook;
-		private delegate void TradeCount(nint a1, int a2, int a3);
-		private void DetourTradeCount(nint a1, int a2, int a3)
-		{
-			PluginLog.Information($"交易 物品槽 数量: {a1:X}, {a2:X}, {a3}");
-			this._tradeCountHook!.Original(a1, a2, a3);
 		}
 	}
 }
---
+++ Cashier/Commons/HookHelper.cs
@@ -0,0 +1,118 @@
+﻿using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Cashier.Commons
+{
+	public class HookHelper : IDisposable
+	{
+		private bool isDisposed;
+
+		private readonly Cashier cashier;
+
+		public HookHelper(Cashier cashier)
+		{
+			this.cashier = cashier;
+			Svc.GameInteropProvider.InitializeFromAttributes(this);
+
+			//_resetSlotHook?.Enable();
+			_setSlotItemIdHook?.Enable();
+			_tradeTargetHook?.Enable();
+			_tradeOtherMoney?.Enable();
+			//_tradeCountHook?.Enable();
+		}
+
+		public void Dispose()
+		{
+			if (isDisposed) 
+				return;
+			isDisposed = true;
+
+			_tradeCountHook?.Dispose();
+			_tradeOtherMoney?.Dispose();
+			_tradeTargetHook?.Dispose();
+			_resetSlotHook?.Dispose();
+			_setSlotItemIdHook?.Dispose();
+		}
+
+
+
+		
+		[Signature("40 53 48 83 EC 20 83 79 70 00 48 8B D9 74 36", DetourName = nameof(DetourResetSlot))]
+		private Hook<ResetSlot>? _resetSlotHook;
+		private delegate nint ResetSlot(nint a);
+		private nint DetourResetSlot(nint a)
+		{
+			Svc.PluginLog.Information($"一个格子被清空: {a:X}");
+
+			try {
+				// your plugin logic goes here.
+			} catch (Exception ex) {
+				Svc.PluginLog.Error("An error occured when handling a macro save event." + ex.Message);
+			}
+
+			return this._resetSlotHook!.Original(a);
+		}
+
+		[Signature("48 89 5C 24 08 48 89 6C 24 10 48 89 74 24 18 48 89 7C 24 20 41 56 48 83 EC 20 8B F2 41 8B E9 48 8B D9 45", DetourName = nameof(DetourSetSlotItemId))]
+		private Hook<SetSlotItemId>? _setSlotItemIdHook;
+		private delegate nint SetSlotItemId(nint a, uint itemId, nint a3, int a4, nint a5);
+		private nint DetourSetSlotItemId(nint a, uint itemId, nint a3, int a4, nint a5)
+		{
+			// 未能读取到物品数量，暂时搁置
+			Svc.PluginLog.Information($"一个格子设置: {a:X}, itemId:{itemId}, a3:{a3}, a4:{a4}, a5:{a5}");
+
+			try {
+				// your plugin logic goes here.
+			} catch (Exception ex) {
+				Svc.PluginLog.Error("An error occured when handling a macro save event." + ex.Message);
+			}
+
+			return this._setSlotItemIdHook!.Original(a, itemId, a3, a4, a5);
+		}
+
+		[Signature("4C 8B DC 53 55 48 81 EC 68 01 00 00 48 8B 05 0D 56 A6 01 48 33 C4 48 89 84 24 40 01 00 00", DetourName = nameof(DetourTradeTarget))]
+		private Hook<TradeTarget>? _tradeTargetHook;
+		private delegate nint TradeTarget(nint a1, nint a2, nint a3);
+		private nint DetourTradeTarget(nint a1, nint objectId, nint a3)
+		{
+			switch (Marshal.ReadByte(a3 + 4)) {
+				case 16:
+					// case 16: 交易状态更新
+					cashier.PluginUi.Trade.SetTradeTarget(objectId);
+					break;
+				case 17:
+					// case 17: 交易成功
+					break;
+				default:
+					Svc.PluginLog.Information($"交易目标: {a1:X}, {objectId:X}, {a3:X}, {Marshal.ReadByte(a3 + 4)}");
+					// case 7: 取消交易
+					// case 1: 别人交易你
+					// case 2: 发起交易
+
+					// case 5: 最终确认
+					break;
+			}
+			return this._tradeTargetHook!.Original(a1, objectId, a3);
+		}
+
+		[Signature("0F B7 42 06 4C 8B D1 44 8B 4A 10 4C 6B C0 38 41 80 BC 08", DetourName = nameof(DetourTradeOtherMoney))]
+		private Hook<TradeOtherMoney>? _tradeOtherMoney;
+		private delegate nint TradeOtherMoney(nint a1, nint a2);
+		private nint DetourTradeOtherMoney(nint a1, nint a2)
+		{
+			Svc.PluginLog.Information($"交易对面出价: {a1:X}, {a2:X}, {(uint)Marshal.ReadInt32(a2 + 8)}");
+			return this._tradeOtherMoney!.Original(a1, a2);
+		}
+
+		[Signature("3B 51 08 7D ?? 48 8B 41 20 48 63 D2 44 39 04 90", DetourName = nameof(DetourTradeCount))]
+		private Hook<TradeCount>? _tradeCountHook;
+		private delegate void TradeCount(nint a1, int a2, int a3);
+		private void DetourTradeCount(nint a1, int a2, int a3)
+		{
+			Svc.PluginLog.Information($"交易 物品槽 数量: {a1:X}, {a2:X}, {a3}");
+			this._tradeCountHook!.Original(a1, a2, a3);
+		}
+	}
+}
