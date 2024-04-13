cb7c1b350c45a350fd97c35f2ae4d662b9c25a08
status102 <102887808+status102@users.noreply.github.com>
2024-04-13 22:15:44 +0800

    refactor: rename

---
+++ Cashier/Models/FFXIV/AgentTrade.cs
@@ -0,0 +1,31 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+using System.Runtime.InteropServices;
+
+namespace Cashier.Models.FFXIV
+{
+    /// <summary>
+    /// 具体大小未知
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, Size = 1408)]
+    public struct AgentTrade
+    {
+
+        [FieldOffset(0)]
+        public AgentInterface AgentInterface;
+
+
+        [FieldOffset(48)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public Slot[] TradeSlot;
+
+        [StructLayout(LayoutKind.Explicit, Size = 136)]
+        public struct Slot
+        {
+            [FieldOffset(0)]
+            public nint Unknown;
+
+            [FieldOffset(112)]
+            public int ItemId;
+        }
+    }
+}
--- Cashier/Models/TradeItem.cs
+++ Cashier/Models/TradeItem.cs
@@ -1,32 +1,33 @@
-﻿using Dalamud.Logging;
+﻿using Cashier.Commons;
+using Cashier.Universalis;
 using Lumina.Excel.GeneratedSheets;
 using System.Linq;
-using Cashier.Commons;
-using Cashier.Universalis;
 
 namespace Cashier.Model
 {
-    public class TradeItem
+	public class TradeItem
 	{
-		public uint Id { get; private init; }
+		public uint Id { get; private init; } = 0;
 		public uint Count { get; set; } = 0;
-		public bool Quality { get; private init; } = false;
-		public ushort? IconId { get; private init; } = 0;
-		public string Name { get; private init; }
-		public uint StackSize { get; private init; } = 1;
-		public Price.ItemPrice ItemPrice { get; private init; }
+		public bool Quality { get; init; } = false;
+		public ushort IconId { get; init; } = 0;
+		public string Name { get; init; } = string.Empty;
+		public uint StackSize { get; init; } = 1;
+		public Price.ItemPrice ItemPrice { get; init; }
 
 		public int MinPrice = 0;
 		public float PresetPrice = 0;
-		public Preset? ItemPreset { get; private init; }
-		public TradeItem() { Id = 0; }
-		public TradeItem(uint id, uint count = 1, bool quality = false) {
+		public Preset? ItemPreset { get; init; }
+
+		public TradeItem() { }
+		public TradeItem(uint id, uint count = 1, bool quality = false)
+		{
 			Id = id;
 			Count = count;
 			Quality = quality;
 			var item = Svc.DataManager.GetExcelSheet<Item>()?.FirstOrDefault(r => r.RowId == id);
 			if (item == null) {
-				Name = "Unknown";
+				Name = "???";
 				Svc.PluginLog.Warning($"获取物品信息错误: id={id}");
 			} else {
 				IconId = item.Icon;
